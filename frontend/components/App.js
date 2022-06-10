b8344086507c96270199fc7bfc2f0e35fd9b9ec5
AnneAugust606 <82916149+gretchyn-hickman@users.noreply.github.com>
2022-06-09 20:11:37 -0600

    to-dos from server

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,40 @@
-import React from 'react'
-
-const URL = 'http://localhost:9000/api/todos'
+import React from "react";
+import axios from "axios";
+const URL = "http://localhost:9000/api/todos";
 
 export default class App extends React.Component {
+  state = {
+    todos: [],
+  };
+  fetchTodo = () => {
+    axios
+      .get(URL)
+      .then((res) => {
+        this.setState({ ...this.state, todos: res.data.data });
+      })
+      .catch((err) => {
+        debugger;
+      });
+  };
+  componentDidMount() {
+    this.fetchTodo();
+  }
+
   render() {
-    return null
+    return (
+      <div>
+        <h1>My To-Do's</h1>
+        <ul>
+          {this.state.todos.map((todo) => {
+            return <li key={todo.id}>{todo.name}</li>;
+          })}
+        </ul>
+        <form>
+          <input />
+          <button>Add Item</button>
+        </form>
+        <button>Clear</button>
+      </div>
+    );
   }
 }
